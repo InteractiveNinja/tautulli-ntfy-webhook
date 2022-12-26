46427b2e9ca06ce3d2519e3e61a71d5123d11b75
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-12-26 12:23:03 +0100

    uses response mapper in POST hook request

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
-import express, { Request, Response } from "express";
-import "dotenv/config";
+import express, { Request, Response } from 'express';
+import 'dotenv/config';
+import { ConfigImpl } from './Config';
+import { ResponseMapper } from './ResponseMapper';
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({
-    message: "hello world",
-  });
+app.use(express.json());
+
+const configReader = ConfigImpl.getConfig();
+const ntfyResponseMapper = new ResponseMapper(configReader);
+
+app.get('status', (req: Request, res: Response) => {
+  res.sendStatus(200);
 });
+
+app.post('/hook', (req: Request, res: Response) => {
+  ntfyResponseMapper
+    .createNtfyResponse(req.body)
+    .then(async (ntfyResponse) => await ntfyResponseMapper.sendNtfyResponse(ntfyResponse))
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
 });
