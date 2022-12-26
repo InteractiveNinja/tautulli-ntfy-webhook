0e957dcc2ca58d5ed14569e3545c317bd9ce3ef3
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-12-26 14:47:00 +0100

    updates webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 module.exports = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
+  target: 'node',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-    extensions: [".webpack.js", ".web.js", ".ts", ".js"],
+    extensions: ['.webpack.js', '.web.js', '.ts', '.js'],
   },
   module: {
-    rules: [{ test: /\.ts$/, loader: "ts-loader" }],
+    rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
   },
   externals: [nodeExternals()],
-  mode: process.env.NODE_ENV ?? "development",
+  mode: 'production',
 };
