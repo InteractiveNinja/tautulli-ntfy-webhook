5b3f98869eeaca3dcd1b2c5e10e22bc09d22c8fb
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-12-26 11:08:02 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".js"],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: "ts-loader" }],
+  },
+  externals: [nodeExternals()],
+  mode: process.env.NODE_ENV ?? "development",
+};
