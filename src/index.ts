afc058a97deb23df6a8906614fb747a7c19df424
InteractiveNinja <gabrielnadolny2@gmail.com>
2023-03-23 11:48:53 +0100

    Adds typedi reflect-metadata

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express, { Request, Response } from 'express';
 import { ConfigLoader } from './config-loader';
 import { ResponseMapper } from './ResponseMapper';
